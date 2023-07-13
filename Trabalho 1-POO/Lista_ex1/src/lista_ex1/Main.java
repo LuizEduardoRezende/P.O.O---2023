851853a2ee2b4fa5cfe66b5116023270106f0e71
LuizEduardoRezende <114539956+LuizEduardoRezende@users.noreply.github.com>
2023-07-13 08:23:17 -0300

    trabalho 1

---
+++ Trabalho 1-POO/Lista_ex1/src/lista_ex1/Main.java
@@ -0,0 +1,14 @@
+
+package lista_ex1;
+//pronta
+public class Main {
+
+    public static void main(String[] args) {
+        CalculadoraCompleta c1 = new CalculadoraCompleta(4,4);
+        System.out.println(c1); 
+        System.out.println(c1.getRaiz(4));
+        System.out.println(c1.getPotencia(4,4));
+        
+    }
+    
+}
